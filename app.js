16494da29092ea577c301b4da055949378981984
Leelun <louie011106@gmail.com>
2023-03-24 20:42:29 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
-const app = express()
 const port = 3000
+const mongoose = require('mongoose')  //載入Mongoose
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+
+}
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //連線到mongoDB的相關設定
+
+
+const db = mongoose.connection // 取得資料庫連線狀態
+
+db.on('error', () => {
+  console.log('mongodb error!')  // 連線異常
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')  // 連線成功
+})
 
 
 app.get('/', (req, res) => {
