1bd5876d758f210cf9dd7fd9a3f5b428678d543f
Leelun <louie011106@gmail.com>
2023-04-01 15:59:04 +0800

    feat:model todo add isDone params

--- app.js
+++ app.js
@@ -71,18 +71,20 @@ app.get('/todos/:id/edit', (req, res) => { //UPDATE功能1
 })
 app.post('/todos/:id/edit', (req, res) => {//UPDATE功能2
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
+    
 })
 
 
-app.post('/todos/:id/delete', (req, res) => {
+app.post('/todos/:id/delete', (req, res) => { //DELETE功能
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
